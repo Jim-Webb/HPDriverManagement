193ca34987112115c17386abfc64566b6c3387e3
Jim Webb <50475234+Jim-Webb@users.noreply.github.com>
2025-02-23 21:41:46 -0500

    Update New-DM_HPRepositoryGlobalIncludeExcludeConfig.ps1

--- Public/New-DM_HPRepositoryGlobalIncludeExcludeConfig.ps1
+++ Public/New-DM_HPRepositoryGlobalIncludeExcludeConfig.ps1
@@ -26,7 +26,6 @@ Function New-DM_HPRepositoryGlobalIncludeExcludeConfig ()
     process
     {
 
-
 $JsonTemplate = @"
 {
     "Win10": {
@@ -42,42 +41,42 @@ $JsonTemplate = @"
 }
 "@
 
-            Write-CMTraceLog -Message "Checking for global include\exclude config file." -Component $Component -type 1 -Logfile $LogFile 
-            $ConfigFile = "$HPRepoPath\$Status\GlobalIncludeExclude.json"
+        Write-CMTraceLog -Message "Checking for global include\exclude config file." -Component $Component -type 1 -Logfile $LogFile 
+        $ConfigFile = "$HPRepoPath\$Status\GlobalIncludeExclude.json"
 
-            Write-CMTraceLog -Message "Config file: $ConfigFile." -Component $Component -type 1 -Logfile $LogFile 
+        Write-CMTraceLog -Message "Config file: $ConfigFile." -Component $Component -type 1 -Logfile $LogFile 
 
-            $SupportedOS = @('Win10','Win11')
+        $SupportedOS = @('Win10','Win11')
 
-            If (-Not (Test-Path $ConfigFile))
+        If (-Not (Test-Path $ConfigFile))
+        {
+            if($PSCmdlet.ShouldProcess("$Status repo folder?", "Create HP global repository include\exclude file."))
             {
-                if($PSCmdlet.ShouldProcess("$Status repo folder?", "Create HP global repository include\exclude file."))
-                {
-                    $Json = $JsonTemplate | ConvertFrom-Json -Verbose
+                $Json = $JsonTemplate | ConvertFrom-Json -Verbose
 
-                    Foreach ($OS in $SupportedOS)
-                    {
-                        Write-CMTraceLog -Message "Adding date modified." -Component $Component -type 1 -Logfile $LogFile 
-                        ($Json.$OS).Modified = Get-Date -Format MM/dd/yyyy
-    
-                        Write-CMTraceLog -Message "Adding author." -Component $Component -type 1 -Logfile $LogFile 
-                        ($Json.$OS).Author = $(whoami)
-            
-                        Write-CMTraceLog -Message "Saving default Json to `"$ConfigFile`"." -Component $Component -type 1 -Logfile $LogFile 
-                        $Json | ConvertTo-Json -Depth 3 | Format-Json | Set-Content $ConfigFile
-            
-                        Write-CMTraceLog -Message "Process complete." -Component $Component -type 1 -Logfile $LogFile 
-                        $ReturnValue = $true
-                    }
+                Foreach ($OS in $SupportedOS)
+                {
+                    Write-CMTraceLog -Message "Adding date modified." -Component $Component -type 1 -Logfile $LogFile 
+                    ($Json.$OS).Modified = Get-Date -Format MM/dd/yyyy
+
+                    Write-CMTraceLog -Message "Adding author." -Component $Component -type 1 -Logfile $LogFile 
+                    ($Json.$OS).Author = $(whoami)
+        
+                    Write-CMTraceLog -Message "Saving default Json to `"$ConfigFile`"." -Component $Component -type 1 -Logfile $LogFile 
+                    $Json | ConvertTo-Json -Depth 3 | Format-Json | Set-Content $ConfigFile
+        
+                    Write-CMTraceLog -Message "Process complete." -Component $Component -type 1 -Logfile $LogFile 
+                    $ReturnValue = $true
                 }
             }
-            else
-            {
-                Write-Warning "Config file `"$ConfigFile`" already exists."
-                Write-CMTraceLog -Message "Config file `"$ConfigFile`" already exists." -Component $Component -type 1 -Logfile $LogFile 
-                $ReturnValue = $false
-            }
+        }
+        else
+        {
+            Write-Warning "Config file `"$ConfigFile`" already exists."
+            Write-CMTraceLog -Message "Config file `"$ConfigFile`" already exists." -Component $Component -type 1 -Logfile $LogFile 
+            $ReturnValue = $false
+        }
         
-    Return $ReturnValue
+        Return $ReturnValue
     }
 }
