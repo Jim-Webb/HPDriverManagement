5805b83e315764b398bbdd906f4d65e62cd4bc19
Jim Webb <50475234+Jim-Webb@users.noreply.github.com>
2025-04-30 22:57:40 -0400

    Update HPDriverManagement.psm1

--- Output/HPDriverManagement/25.4.30.1/HPDriverManagement.psm1
+++ Output/HPDriverManagement/25.4.30.1/HPDriverManagement.psm1
@@ -2092,22 +2092,24 @@ Function Get-DM_PSProfileMods ()
 {
     Write-host "In order to prevent being prompted for required variables when running the cmdlets, you can add the following to your powershell profile."
 
-    Write-Host "You can add the following to `"$($PROFILE.CurrentUserAllHosts)`" to only effect your profile or"
-    Write-host "add it to `"$PSHOME\Profile.ps1`" to make them available for all users on this system. This option requires Administrative permissions. :-("
-    Write-Host ""
-    Write-Host "###################################################################################"
-    Write-host '$PSDefaultParameterValues["*-DM*:SiteServer"] = "smsprov.corp.viamonstra.com"'
-    Write-host '$PSDefaultParameterValues["*-DM*:SiteCode"] = "HOS"'
-    write-host '$PSDefaultParameterValues["*-DM*:CMDBServer"] = "sccmdb.corp.viamonstra.com"'
-    write-host '$PSDefaultParameterValues["*-DM*:HPRepoPath"] = "\\corp.viamonstra.com\SourceFiles$\WorkstationDriverRepository"'
-    write-host '$PSDefaultParameterValues["*-DM*:PackageContentPath"] = "\\corp.viamonstra.com\SourceFiles$\OSD\Drivers\DriverPacks"'
-    Write-Host ""
-    Write-Host "# This command creates a central log file for the driver management process. If this is not set, each function will have it's own log."
-    write-host '$PSDefaultParameterValues["*-DM*:LogFile"] = "C:\ProgramData\Logs\DriverManagement.log"'
-    Write-Host "###################################################################################"
+    Write-Host  "You can add the following to `"$($PROFILE.CurrentUserAllHosts)`" to only effect your profile or"
+    Write-host  "add it to `"$PSHOME\Profile.ps1`" to make them available for all users on this system. This option requires Administrative permissions. :-("
+    Write-Host  ""
+    Write-Host  "###################################################################################"
+    Write-host  '$PSDefaultParameterValues["*-DM*:SiteServer"] = "smsprov.corp.viamonstra.com"'
+    Write-host  '$PSDefaultParameterValues["*-DM*:SiteCode"] = "PS1"'
+    write-host  '$PSDefaultParameterValues["*-DM*:CMDBServer"] = "sccmdb.corp.viamonstra.com"'
+    write-host  '$PSDefaultParameterValues["*-DM*:HPRepoPath"] = "\\corp.viamonstra.com\SourceFiles$\WorkstationDriverRepository"'
+    write-host  '$PSDefaultParameterValues["*-DM*:PackageContentPath"] = "\\corp.viamonstra.com\SourceFiles$\OSD\Drivers\DriverPacks"'
+	Write-Host  '$PSDefaultParameterValues["*-DM*:DistributionPointGroupName"] = "All DPs"'
+	Write-Host  '$PSDefaultParameterValues["*-DM*:CMFolder"] = "PS1:\Package\OSD\HP"'
+    Write-Host  ""
+    Write-Host  "# This command creates a central log file for the driver management process. If this is not set, each function will have it's own log."
+    write-host  '$PSDefaultParameterValues["*-DM*:LogFile"] = "C:\ProgramData\Logs\DriverManagement.log"'
+    Write-Host  "###################################################################################"
 
 }
-#EndRegion '.\Public\Get-DM_PSProfileMods.ps1' 20
+#EndRegion '.\Public\Get-DM_PSProfileMods.ps1' 22
 #Region '.\Public\Get-DM_SystemInformation.ps1' -1
 
 function Get-DM_SystemInformation ()
@@ -3233,9 +3235,9 @@ Function Invoke-DM_HPIARefFileExclude ()
             }
         }
 
-        $HPRepoPath = Join-Path $HPRepoPath "HP"
-
         If ((Invoke-PathPermissionsCheck -Path $HPRepoPath) -eq $false) { break }
+
+        # $HPRepoPath = Join-Path $HPRepoPath "HP"
     }
 
     Process
