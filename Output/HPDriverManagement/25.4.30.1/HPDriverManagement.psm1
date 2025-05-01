1905e067c540b439594edd8f2ac20b885fe2cbd3
Jim Webb <50475234+Jim-Webb@users.noreply.github.com>
2025-04-30 23:11:58 -0400

    Update HPDriverManagement.psm1

--- Output/HPDriverManagement/25.4.30.1/HPDriverManagement.psm1
+++ Output/HPDriverManagement/25.4.30.1/HPDriverManagement.psm1
@@ -1931,7 +1931,7 @@ Function Get-DM_HPRepositoryIncludeExclude ()
 
         $PSDefaultParameterValues = $Global:PSDefaultParameterValues
 
-        $HPRepoPath = Join-Path $HPRepoPath "HP"
+        # $HPRepoPath = Join-Path $HPRepoPath "HP"
 
         Write-CMTraceLog -Message "------ Start ------" -Component $Component -type 1 -Logfile $LogFile 
 
